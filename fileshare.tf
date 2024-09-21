28ff3b054b6bbfb9091526f58b9920a5f2f4b8f9
Douglas Francis <douglas@poshops.io>
2024-09-20 19:52:01 -0500

    fmt

--- fileshare.tf
+++ fileshare.tf
@@ -1,6 +1,7 @@
 resource "azurerm_storage_share" "main" {
-  for_each             = { for share in var.storage_shares : share.name => share }
-  name                 = each.value.name
+  for_each = { for share in var.storage_shares : share.name => share }
+  name     = each.value.name
+
   storage_account_name = azurerm_storage_account.main.name
   quota                = each.value.quota
 }
